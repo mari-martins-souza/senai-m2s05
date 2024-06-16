9f85def48cf0bcab8aff441f2bb0e05f6f4b0a51
Mariana Martins <marih_ms@hotmail.com>
2024-06-16 12:31:07 -0300

    insert data

--- Exercícios SQL M2S05.sql
+++ Exercícios SQL M2S05.sql
@@ -26,3 +26,36 @@ CREATE TABLE Paciente (
 	telefone varchar(15),
 	email varchar(50)
 );
+
+-- exercicio 3 - DML Insert das Tabelas
+
+INSERT INTO Nutricionista (crm, nome, especialidade, tempo_experiencia) VALUES 
+('CRM123', 'Dr. João Cabral', 'Nutrição Esportiva', 5)
+RETURNING id_nutricionista;
+
+INSERT INTO Nutricionista (crm, nome, especialidade, tempo_experiencia) VALUES 
+('CRM456', 'Mariana Martins', 'Nutrição Infantil', 3)
+RETURNING id_nutricionista;
+
+INSERT INTO Nutricionista (crm, nome, especialidade, tempo_experiencia) VALUES 
+('CRM789', 'Diego Campos', 'Nutrição para Hipertrofia', 8)
+RETURNING id_nutricionista;
+
+INSERT INTO Paciente (nome, data_nascimento, cpf, telefone, email) VALUES
+('Carlos Silva', '1980-05-20', '12345678901', '11987654321', 'carlos.silva@email.com')
+RETURNING id_paciente;
+
+INSERT INTO Paciente (nome, data_nascimento, cpf, telefone, email) VALUES
+('Ana Pereira', '1992-10-30', '23456789012', '21987654321', 'ana.pereira@email.com')
+RETURNING id_paciente;
+
+INSERT INTO Paciente (nome, data_nascimento, cpf, telefone, email) VALUES
+('Pedro Costa', '1975-07-15', '34567890123', '31987654321', 'pedro.costa@email.com')
+RETURNING id_paciente;
+
+INSERT INTO Consulta (id_nutricionista, id_paciente, data_consulta, observacoes) VALUES 
+(1, 3, '2024-06-20', 'Primeira consulta, paciente em bom estado de saúde'),
+(2, 2, '2024-06-21', 'Paciente com necessidade de dieta para perda de peso'),
+(3, 1, '2024-06-22', 'Paciente com necessidade de dieta para ganho de massa muscular');
+
+
